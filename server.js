5620e4ab47639d7f912b62494c51e46a268dea69
HipsterBrown <headhipster@hipsterbrown.com>
2014-11-06 21:53:03 -0500

    refactor server.js to just serve output.json file generated by seperate scraper scrips

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 var Hapi = require('hapi');
-//var cors = require('cors');
-var fs = require('fs');
 var path = require('path');
-var request = require('request');
-var cheerio = require('cheerio');
+var fs = require('fs');
+
 
 var port = process.env.PORT || 8081;
 
 var app = Hapi.createServer('localhost', port, {
   cors: true
 });
-
-// app.use(cors());
-//
-// app.set('port', (process.env.PORT || 8081));
-
 
 app.route({
   method: 'GET',
@@ -24,191 +17,20 @@ app.route({
   }
 });
 
-// app.get('/', function(req, res) {
-//   res.send('You can find Disney Times at /times.');
-// });
-
 app.route({
   method: 'GET',
   path: '/times',
   handler: function(req, reply) {
+    var timesJSON = fs.readFileSync('./output.json', 'utf8');
 
-    var worldUrl = 'https://disneyworld.disney.go.com/calendars/';
-    var landUrl = 'https://disneyland.disney.go.com/calendars/park-hours/';
-
-    /**************
-      Data Model
-
-      {
-        id: Number,
-        title: String,
-        open: String,
-        until: String
-      }
-    *************/
-
-    var json = {
-      "wdw": {
-        "hours": []
-      },
-      "dl": {
-        "hours": []
-      }
-    };
-
-    request(worldUrl, function(error, response, html) {
-      if(!error) {
-        var $ = cheerio.load(html);
-
-        $('.clickRow').each(function(i, elem){
-          var data = $(this);
-          var range = data.find('.hours.range > p').text();
-          var rangeSplit = range.split(' ');
-
-          json.wdw.hours[i] = {
-            "id": (i++).toString(),
-            "title": data.find('.parkName').text(),
-            "open": rangeSplit[0],
-            "until": rangeSplit[2]
-          };
-        });
-
-      }
-
-      fs.appendFile('output.json', JSON.stringify(json, null, 4), function(err) {
-        console.log('File successfully written! - Check your project directory for the output.json file');
-
-
-        request(landUrl, function(error, response, html) {
-          if(!error) {
-            var $ = cheerio.load(html);
-
-            var cards = $('body');
-
-
-            $('.parkHoursList li').each(function(i, elem){
-              var data = $(elem);
-              var range = data.find('.hours.operating').text();
-              var rangeSplit = range.split(' to ');
-
-              var title = data.find('.pkTitle').text().trim();
-                  title = title.slice(0, title.indexOf('Park')).trim();
-
-
-              json.dl.hours[i] = {
-                "id": (i++).toString(),
-                "title": title,
-                "open": rangeSplit[0],
-                "until": rangeSplit[1]
-              };
-            });
-          }
-
-          fs.appendFile('output.json', JSON.stringify(json, null, 4), function(err) {
-            console.log('File successfully written! - Check your project directory for the output.json file');
-          });
-
-          //res.json(json);
-          reply(JSON.stringify(json));
-        });
-      });
-
-    });
-
+    reply(timesJSON);
   }
 });
 
-// app.get('/times', function(req, res) {
-//
-//   var worldUrl = 'https://disneyworld.disney.go.com/calendars/';
-//   var landUrl = 'https://disneyland.disney.go.com/calendars/park-hours/';
-//
-//   /**************
-//     Data Model
-//
-//     {
-//       id: Number,
-//       title: String,
-//       open: String,
-//       until: String
-//     }
-//   *************/
-//
-//   var json = {
-//     "wdw": {
-//       "hours": []
-//     },
-//     "dl": {
-//       "hours": []
-//     }
-//   };
-//
-//   request(worldUrl, function(error, response, html) {
-//     if(!error) {
-//       var $ = cheerio.load(html);
-//
-//       $('.clickRow').each(function(i, elem){
-//         var data = $(this);
-//         var range = data.find('.hours.range > p').text();
-//         var rangeSplit = range.split(' ');
-//
-//         json.wdw.hours[i] = {
-//           "id": (i++).toString(),
-//           "title": data.find('.parkName').text(),
-//           "open": rangeSplit[0],
-//           "until": rangeSplit[2]
-//         };
-//       });
-//
-//     }
-//
-//     fs.appendFile('output.json', JSON.stringify(json, null, 4), function(err) {
-//       console.log('File successfully written! - Check your project directory for the output.json file');
-//
-//
-//       request(landUrl, function(error, response, html) {
-//         if(!error) {
-//           var $ = cheerio.load(html);
-//
-//           var cards = $('body');
-//
-//
-//           $('.parkHoursList li').each(function(i, elem){
-//             var data = $(elem);
-//             var range = data.find('.hours.operating').text();
-//             var rangeSplit = range.split(' to ');
-//
-//             var title = data.find('.pkTitle').text().trim();
-//                 title = title.slice(0, title.indexOf('Park')).trim();
-//
-//
-//             json.dl.hours[i] = {
-//               "id": (i++).toString(),
-//               "title": title,
-//               "open": rangeSplit[0],
-//               "until": rangeSplit[1]
-//             };
-//           });
-//         }
-//
-//         fs.appendFile('output.json', JSON.stringify(json, null, 4), function(err) {
-//           console.log('File successfully written! - Check your project directory for the output.json file');
-//         });
-//
-//         res.json(json);
-//       });
-//     });
-//
-//   });
-//
-// });
 
 app.start(function(){
   console.log('Magic happens on port: ' + app.info.uri);
 });
 
-// app.listen(app.get('port'), function() {
-//   console.log('Magic happens on port: ' + app.get('port'));
-// });
 
 exports = module.exports = app;
