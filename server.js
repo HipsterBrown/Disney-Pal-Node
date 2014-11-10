c8a864f35f066c766b4d9dfb3383f122ec457620
HipsterBrown <headhipster@hipsterbrown.com>
2014-11-09 23:14:03 -0500

    fix caching through s3 and serving file through /times

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var Hapi = require('hapi');
 var path = require('path');
 var fs = require('fs');
+var AWS = require('aws-sdk');
 
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET;
+
+AWS.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
+AWS.config.region = 'us-east-1';
+var s3 = new AWS.S3();
+var params = {
+  Bucket: 'disneypal',
+  Key: 'times'
+};
 
 var port = process.env.PORT || 8081;
 
@@ -21,13 +33,24 @@ app.route({
   method: 'GET',
   path: '/times',
   handler: function(req, reply) {
-    var timesJSON = fs.readFileSync(path.join(__dirname, './output.json'), 'utf8');
+    var timesJSON = fs.createWriteStream('times.json');
 
-    console.log('Server.js - ' + __dirname);
+    //timesJSON = JSON.parse(timesJSON);
 
-    timesJSON = JSON.parse(timesJSON);
+    timesJSON.on('pipe', function(src) {
+      console.log('Piping, piping, piping...');
+    });
 
-    reply(timesJSON);
+    timesJSON.on('finish', function(){
+      console.log('Finished writing file.');
+      reply.file('times.json');
+    });
+
+    s3.getObject(params).createReadStream().pipe(timesJSON);
+
+    //reply.file(times.json);
+
+    console.log('Replying with file stream.');
   }
 });
 
